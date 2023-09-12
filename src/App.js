ba909c82f1e9025a74a130fc72ebeeb7cae9a57a
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-09-12 12:40:53 +0300

    first page html

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React by Sergey Moshkov on CodeSandbox with React Hooks and Context
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <h1 className="mt-3">Программирование для каждого</h1>
+        <hr className="mb-3"></hr>
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <a href="/">Home</a>
+                </li>
+                <li className="list-group-item">
+                <a href="/movies">Movies</a>
+                </li>
+                <li className="list-group-item">
+                <a href="/admin">Manage catalogue</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+            <h2>Context</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
