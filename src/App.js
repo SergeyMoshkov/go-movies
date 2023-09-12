287dd621d7481a616fe302a44d570b00f17aa803
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-09-12 21:52:04 +0300

    fix dependens in App and Movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
 import Home from './components/Home';
 import Movies from './components/Movies';
